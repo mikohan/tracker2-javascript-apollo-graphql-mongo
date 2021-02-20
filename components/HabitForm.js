595692f27f8f69b09cd7db24a0707ccac1ff27ee
mikohan <angara99@gmail.com>
2021-02-20 15:26:44 +0300

    Fixed some issues

--- components/HabitForm.js
+++ components/HabitForm.js
@@ -1,10 +1,28 @@
 import { Form, Field } from '@leveluptuts/fresh';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-const HabitForm = ({ setHabits }) => {
+const ADD_HABIT = gql`
+  mutation addHabit($habit: HabitInput) {
+    addHabit(habit: $habit) {
+      _id
+      name
+    }
+  }
+`;
+
+const HabitForm = () => {
+  const [addHabit] = useMutation(ADD_HABIT);
   return (
     <Form
-      onSubmit={data => {
-        setHabits(prevState => [...prevState, data.habit]);
+      onSubmit={(data) => {
+        addHabit({
+          varables: {
+            habit: {
+              name: data.habit,
+            },
+          },
+        });
       }}
     >
       <Field>Habit</Field>
