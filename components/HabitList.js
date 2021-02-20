5e4b6a7808c02a557651cb7c064d55dee05d12d4
mikohan <angara99@gmail.com>
2021-02-20 14:55:14 +0300

    Initial commit

---
+++ components/HabitList.js
@@ -0,0 +1,14 @@
+import Habit from './Habit';
+
+const HabitList = ({ habits }) => {
+  return (
+    <section>
+      <h2>My Habits</h2>
+      {habits.map((habit, index) => (
+        <Habit key={habit} habit={habit} index={index} />
+      ))}
+    </section>
+  );
+};
+
+export default HabitList;
